a82342d63f9b7129cf18e2cb035ec1acf6ad80ea
gooo000 <gooo000@free.com>
2014-09-26 13:42:59 +0800

    add some code about mq and redis

--- storage/redis.go
+++ storage/redis.go
@@ -1,30 +1,84 @@
 package storage
+//package main
 
 import (
 	//"time"
+	"log"
+	"time"
+	"strconv"
+	"github.com/garyburd/redigo/redis"
 	"github.com/chenyf/push/error"
 )
 
-type RedisStorage struct {
+const RedisServer = "127.0.0.1:6379"
 
+type RedisStorage struct {
+	pool *redis.Pool
+}
+
+func newRedisStorage() *RedisStorage {
+	return &RedisStorage {
+    		pool: &redis.Pool{
+        		MaxIdle: 1,
+        		IdleTimeout: 300 * time.Second,
+        		Dial: func() (redis.Conn, error) {
+        			c, err := redis.Dial("tcp", RedisServer)
+        			if err != nil {
+        				log.Printf("faild to connect Redis:", err)
+        				return nil, err
+        			}
+        			return c, err
+        		},
+        		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+        			_, err := c.Do("PING")
+        			return err
+        		},
+        	},
+	}
 }
 
 // 从存储后端获取 > 指定时间的所有消息
-func (storage RedisStorage)GetOfflineMsgs(appId string, lastMsgId int64) []*PushMessage {
-	msg_list := []*PushMessage{}
-	return msg_list
+func (r *RedisStorage)GetOfflineMsgs(appId string, msgId int64) []string {
+	ret, err := redis.Strings(r.pool.Get().Do("HGETALL", appId))
+	if err != nil {
+		log.Printf("failed to get offline msg:", err)
+		return nil
+	}
+	var msg []string
+	for i := range ret {
+		//log.Print(ret)
+		if _, err := strconv.Atoi(ret[i]); err == nil{
+			continue
+		} else {
+			msg = append(msg, ret[i])
+		}
+	}
+	return msg
 }
 
 // 从存储后端获取指定消息
-func (storage RedisStorage)GetMsg(appId string, msgId int64) *PushMessage {
+func (r *RedisStorage)GetMsg(appId string, msgId int64) string {
+	msg, err := redis.String(r.pool.Get().Do("HGET", appId, msgId))
+	if err != nil {
+		log.Printf("failed to get msg:", err)
+		return ""
+	}
+	return msg
+}
+
+
+func (r *RedisStorage)GetApp(regId string) (*AppInfo) {
 	return nil
 }
 
-func (storage RedisStorage)GetApp(appId string, regId string) (*AppInfo) {
-	return nil
-}
-
-func (storage RedisStorage)AddApp(appId string, regId string, appKey string, devId string) error {
+func (r *RedisStorage)AddApp(regId string, appId string, appKey string, devId string) error {
 	return &pusherror.PushError{"add failed"}
 }
 
+/*
+func main() {
+	r := newRedisStorage()
+	log.Print(r.GetMsg("12345678", 1))
+	log.Print(r.GetOfflineMsgs("12345678", 1))
+}
+*/
--- storage/storage.go
+++ storage/storage.go
@@ -9,24 +9,16 @@ type PushMessage struct {
 }
 
 type AppInfo struct{
-	AppKey		string
-	DevId		string
 	LastMsgId	int64
 }
 type Storage interface {
-	// 获取离线消息
-	GetOfflineMsgs(appId string, lastMsgId int64) []*PushMessage
-	// 获取指定消息
-	GetMsg(appId string, msgId int64) *PushMessage
-
-	// 获取注册app的持久化信息
-	GetApp(appId string, regId string) (*AppInfo)
-
-	// 增加一个注册app
-	AddApp(appId string, regId string, appKey string, devId string) error
+	GetOfflineMsgs(appId string, ctime int64) []string
+	GetMsg(appId string, msgId int64) string
+	GetApp(regId string) (*AppInfo)
+	AddApp(regId string, appId string, appKey string, devId string) error
 }
 
 var (
-	StorageInstance Storage = RedisStorage{}
+	StorageInstance Storage = newRedisStorage()
 )
 
