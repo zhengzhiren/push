f7998d917e46d77d81384bc15b246252a1b25020
chenyf <rstevens2008@hotmail.com>
2014-10-09 14:49:49 +0800

    update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 go build -o pushd
 [ $? -ne 0 ] && { echo "go build failed"; exit 1; }
-cd test && go build -o pushtest
+cd pushtest && go build -o pushtest
 [ $? -ne 0 ] && { echo "go build failed"; exit 1; }
 cd -
 
@@ -10,7 +10,7 @@ mkdir -p output
 rm -rf output/*
 cp pushd output/
 cp setupenv.sh output/
-cp test/pushtest output/
+cp pushtest/pushtest output/
 cp -aR etc output/
 cp -aR bin output/
 
