b289bbdd8fba8f6cb96ec77681ff7b2d3299f37c
chenyf <rstevens2008@hotmail.com>
2014-10-09 11:23:24 +0800

    up

--- setupenv.sh
+++ setupenv.sh
@@ -4,8 +4,8 @@ USER=work
 WORK_ROOT=/letv
 
 mkdir -p $WORK_ROOT/log/push/server
-chown -r $USER:$USER $WORK_ROOT/log/push/server
+chown -R $USER:$USER $WORK_ROOT/log/push/server
 
 mkdir -p $WORK_ROOT/run/push/server
-chown -r $USER:$USER $WORK_ROOT/run/push/server
+chown -R $USER:$USER $WORK_ROOT/run/push/server
 
