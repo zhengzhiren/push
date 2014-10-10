cc98192e51c931b5e4fec54b1cfbcc5443bb57f0
chenyf <rstevens2008@hotmail.com>
2014-10-10 14:37:54 +0800

    update

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-go build -o pushd
-[ $? -ne 0 ] && { echo "go build failed"; exit 1; }
-cd pushtest && go build -o pushtest
-[ $? -ne 0 ] && { echo "go build failed"; exit 1; }
-cd -
-
 mkdir -p output
 rm -rf output/*
-cp pushd output/
-cp setupenv.sh output/
-cp pushtest/pushtest output/
+
+cd server && go build -o ../output/pushd
+[ $? -ne 0 ] && { echo "go build failed"; exit 1; }
+cd - >/dev/null
+
+cd pushtest && go build -o ../output/pushtest
+[ $? -ne 0 ] && { echo "go build failed"; exit 1; }
+cd - >/dev/null
+
+cd api && go build -o ../output/pushapi
+[ $? -ne 0 ] && { echo "go build failed"; exit 1; }
+cd - >/dev/null
+
+cp misc/* output/
 cp -aR etc output/
-cp -aR bin output/
 
