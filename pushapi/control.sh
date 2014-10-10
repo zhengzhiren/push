d3bdf432b41a3bf64a179e7ee0403babbc07116f
chenyf <rstevens2046@gmail.com>
2014-10-10 23:27:56 +0800

    up

---
+++ pushapi/control.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+cmd=$1
+
+THIS_DIR=$(dirname $(readlink -f $0) )
+
+function start()
+{
+	sleep 3
+	supervisord -c $THIS_DIR/conf/supervisord.conf
+	[ $? -ne 0 ] && { echo "start supervisord failed"; exit 1; }
+	retry=0
+	while [ $retry -lt 5 ]; do
+		supervisorctl -c $THIS_DIR/conf/supervisord.conf status pushapi |grep RUNNING >/dev/null
+		[ $? -eq 0 ] && { break; }
+		retry=$(($retry+1))
+		sleep 1
+	done
+	[ $? -ge 5 ] && { echo "push api not in running status"; return 1; }
+	return 0
+}
+
+function stop()
+{
+	supervisorctl -c $THIS_DIR/conf/supervisord.conf shutdown >/dev/null 2>&1
+	return 0
+	#ps axf|grep supervisord |grep push-server
+	
+}
+
+function restart()
+{
+	stop
+	start
+}
+
+case $cmd in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;	
+	restart)
+		restart
+		;;
+	*)
+		echo $"Usage: $0 {start|stop|restart}"
+		RET=2	
+esac
+exit $RET
+
