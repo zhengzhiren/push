cce4a22c707d02669b51b4578d9e1e01a7af1be2
zhengzhiren <zhengzhiren@gmail.com>
2014-10-13 16:12:26 +0800

    change read method when receiving packet data change InitReplyMessage.Result to int

--- comet/message.go
+++ comet/message.go
@@ -6,32 +6,32 @@ import (
 )
 
 type Header struct {
-	Type	uint8
-	Ver		uint8
-	Seq		uint32
-	Len		uint32
+	Type uint8
+	Ver  uint8
+	Seq  uint32
+	Len  uint32
 }
 
 type Message struct {
-	Header	Header
-	Data	[]byte
+	Header Header
+	Data   []byte
 }
 
 const (
-	HEADER_SIZE				= uint32(10)
-	MSG_HEARTBEAT			= uint8(0)
-	MSG_INIT				= uint8(1)
-	MSG_INIT_REPLY			= uint8(2)
-	MSG_REGISTER			= uint8(3)
-	MSG_REGISTER_REPLY		= uint8(4)
-	MSG_UNREGISTER			= uint8(5)
-	MSG_UNREGISTER_REPLY	= uint8(6)
-	MSG_PUSH				= uint8(10)
-	MSG_PUSH_REPLY			= uint8(11)
+	HEADER_SIZE          = uint32(10)
+	MSG_HEARTBEAT        = uint8(0)
+	MSG_INIT             = uint8(1)
+	MSG_INIT_REPLY       = uint8(2)
+	MSG_REGISTER         = uint8(3)
+	MSG_REGISTER_REPLY   = uint8(4)
+	MSG_UNREGISTER       = uint8(5)
+	MSG_UNREGISTER_REPLY = uint8(6)
+	MSG_PUSH             = uint8(10)
+	MSG_PUSH_REPLY       = uint8(11)
 )
 
 // msg to byte
-func (header *Header)Serialize() ([]byte, error) {
+func (header *Header) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
 	if err := binary.Write(buf, binary.BigEndian, *header); err != nil {
 		return nil, err
@@ -40,7 +40,7 @@ func (header *Header)Serialize() ([]byte, error) {
 }
 
 // byte to msg
-func (header *Header)Deserialize(b []byte) (error) {
+func (header *Header) Deserialize(b []byte) error {
 	buf := bytes.NewReader(b)
 	if err := binary.Read(buf, binary.BigEndian, header); err != nil {
 		return err
@@ -49,43 +49,42 @@ func (header *Header)Deserialize(b []byte) (error) {
 }
 
 type InitMessage struct {
-	DeviceId	string				`json:"devid"`
-	Apps		[]RegisterMessage	`json:"apps"`
+	DeviceId string            `json:"devid"`
+	Apps     []RegisterMessage `json:"apps"`
 }
 type InitReplyMessage struct {
-	Result	string `json:"result"`
+	Result int `json:"result"`
 }
-type RegisterMessage struct{
-	AppId	string	`json:"appid"`
-	AppKey	string	`json:"appkey"`
-	RegId	string	`json:"regid"`
-	Token	string	`json:"token"`
+type RegisterMessage struct {
+	AppId  string `json:"appid"`
+	AppKey string `json:"appkey"`
+	RegId  string `json:"regid"`
+	Token  string `json:"token"`
 }
-type RegisterReplyMessage struct{
-	AppId	string	`json:"appid"`
-	RegId	string	`json:"regid"`
-	Result	int		`json:"result"`
+type RegisterReplyMessage struct {
+	AppId  string `json:"appid"`
+	RegId  string `json:"regid"`
+	Result int    `json:"result"`
 }
-type UnregisterMessage struct{
-	AppId	string	`json:"appid"`
-	AppKey	string	`json:"appkey"`
-	RegId	string	`json:"regid"`
-	Token	string	`json:"token"`
+type UnregisterMessage struct {
+	AppId  string `json:"appid"`
+	AppKey string `json:"appkey"`
+	RegId  string `json:"regid"`
+	Token  string `json:"token"`
 }
-type UnregisterReplyMessage struct{
-	AppId	string	`json:"appid"`
-	RegId	string	`json:"regid"`
-	Result	int		`json:"result"`
+type UnregisterReplyMessage struct {
+	AppId  string `json:"appid"`
+	RegId  string `json:"regid"`
+	Result int    `json:"result"`
 }
 type PushMessage struct {
-	MsgId		int64	`json:"msgid"`
-	AppId		string	`json:"appid"`
-	Type		int		`json:"type"`  //1: notification  2:app message
-	Content		string	`json:"content"`
+	MsgId   int64  `json:"msgid"`
+	AppId   string `json:"appid"`
+	Type    int    `json:"type"` //1: notification  2:app message
+	Content string `json:"content"`
 }
 type PushReplyMessage struct {
-	MsgId	int64	`json:"msgid"`
-	AppId	string	`json:"appid"`
-	RegId	string	`json:"regid"`
+	MsgId int64  `json:"msgid"`
+	AppId string `json:"appid"`
+	RegId string `json:"regid"`
 }
-
