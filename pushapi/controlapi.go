5f5a3ee14f1af645cced32526d021521869e8c44
zhengzhiren <zhengzhiren@gmail.com>
2014-12-02 16:50:43 +0800

    add compatible API

--- pushapi/controlapi.go
+++ pushapi/controlapi.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/ant0ine/go-json-rest/rest"
 	log "github.com/cihub/seelog"
+	"github.com/fighterlyt/permutation"
 
 	"github.com/chenyf/push/cloud"
 	"github.com/chenyf/push/devcenter"
@@ -118,10 +123,6 @@ func getDeviceList(w rest.ResponseWriter, r *rest.Request) {
 
 func getDevice(w rest.ResponseWriter, r *rest.Request) {
 	devId := r.PathParam("devid")
-	//	if !comet.DevMap.Check(devId) {
-	//		rest.NotFound(w, r)
-	//		return
-	//	}
 
 	r.ParseForm()
 	token := r.FormValue("token")
@@ -136,10 +137,17 @@ func getDevice(w rest.ResponseWriter, r *rest.Request) {
 		return
 	}
 
-	resp := cloud.ApiResponse{}
-	resp.ErrNo = cloud.ERR_NOERROR
-	//resp.Data = devInfo
-	w.WriteJson(resp)
+	if serverName, err := storage.Instance.CheckDevice(devId); err == nil && serverName != "" {
+		resp := cloud.ApiResponse{}
+		resp.ErrNo = cloud.ERR_NOERROR
+		resp.Data = devInfo{
+			Id: devId,
+		}
+		w.WriteJson(resp)
+	} else {
+		rest.NotFound(w, r)
+		return
+	}
 }
 
 func controlDevice(w rest.ResponseWriter, r *rest.Request) {
@@ -194,4 +202,390 @@ func controlDevice(w rest.ResponseWriter, r *rest.Request) {
 	}
 
 	w.WriteJson(resp)
+}
+
+//
+// compatible APIs
+//
+const (
+	STATUS_OK                = 0
+	STATUS_OTHER_ERR         = -1
+	STATUS_ROUTER_NOT_LOGGIN = -2
+	STATUS_TASK_NOT_EXIST    = -3
+	STATUS_TASK_EXIST        = -4
+	STATUS_INVALID_PARAM     = -5
+	STATUS_ROUTER_OFFLINE    = -6
+)
+
+type CommandResponse struct {
+	Status int    `json:"status"`
+	Error  string `json:"error"`
+}
+
+var (
+	permutations [][]int
+)
+
+func initPermutation() error {
+	a := []int{0, 1, 2, 3, 4, 5, 6}
+
+	p, err := permutation.NewPerm(a, nil)
+	if err != nil {
+		return err
+	}
+
+	result := p.NextN(p.Left())
+	permutations = result.([][]int)
+
+	log.Infof("Generated %d permutations", len(permutations))
+
+	return nil
+}
+
+func sign_calc(path string, query map[string]string) string {
+	const key string = "xnRzFxoCDRVRU2mNQ7AoZ5MCxpAR7ntnmlgRGYav"
+
+	uid := query["uid"]
+	rid := query["rid"]
+	tid := query["tid"]
+	src := query["src"]
+	tm := query["tm"]
+	pmtt := query["pmtt"]
+
+	raw := []string{path, uid, rid, tid, src, tm, pmtt}
+	perm_index, err := strconv.Atoi(pmtt)
+	if err != nil || perm_index < 0 || perm_index >= len(permutations) {
+		return ""
+	}
+
+	perm := permutations[perm_index]
+	//log.Debugf("perm: %v", perm)
+	args := []string{}
+	for _, item := range perm {
+		args = append(args, raw[item])
+	}
+	data := strings.Join(args, "")
+	//log.Debugf(data)
+	mac := hmac.New(sha1.New, []byte(key))
+	mac.Write([]byte(data))
+	return hex.EncodeToString(mac.Sum(nil))
+}
+
+func checkAuthzUid(uid string, devid string) bool {
+	// TODO: remove this is for test
+	if uid == "000000000" {
+		return true
+	}
+
+	devices, err := devcenter.GetDevices(uid, devcenter.DEV_ROUTER)
+	if err != nil {
+		log.Errorf("GetDevices failed: %s", err.Error())
+		return false
+	}
+
+	for _, dev := range devices {
+		if devid == dev.Id {
+			return true
+		}
+	}
+	return false
+}
+
+func postRouterCommand(w http.ResponseWriter, r *http.Request) {
+	log.Debugf("Request from RemoterAddr: %s", r.RemoteAddr)
+	var (
+		uid         string
+		rid         string
+		tid         string
+		sign        string
+		tm          string
+		src         string
+		pmtt        string
+		path        string = "/router/command"
+		mysign      string
+		query       map[string]string
+		response    CommandResponse
+		serviceName string
+		cmd         string
+		result      string
+		body        []byte
+		err         error
+		bCmd        []byte
+	)
+	response.Status = STATUS_OK
+	if r.Method != "POST" {
+		response.Status = STATUS_OTHER_ERR
+		response.Error = "must use 'POST' method\n"
+		goto resp
+	}
+
+	r.ParseForm()
+	rid = r.FormValue("rid")
+	if rid == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Error = "missing 'rid'"
+		goto resp
+	}
+
+	uid = r.FormValue("uid")
+	if uid == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Error = "missing 'uid'"
+		goto resp
+	}
+
+	tid = r.FormValue("tid")
+	if tid == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Error = "missing 'tid'"
+		goto resp
+	}
+
+	sign = r.FormValue("sign")
+	if sign == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Error = "missing 'sign'"
+		goto resp
+	}
+
+	tm = r.FormValue("tm")
+	if tm == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Error = "missing 'tm'"
+		goto resp
+	}
+
+	pmtt = r.FormValue("pmtt")
+	if pmtt == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Error = "missing 'pmtt'"
+		goto resp
+	}
+
+	src = r.FormValue("src")
+	if src == "" {
+		src = "src"
+	}
+
+	query = map[string]string{
+		"uid":  uid,
+		"rid":  rid,
+		"tid":  tid,
+		"src":  src,
+		"tm":   tm,
+		"pmtt": pmtt,
+	}
+	mysign = sign_calc(path, query)
+	if mysign != sign {
+		log.Warnf("sign valication failed")
+		log.Warnf("mysign: %s", mysign)
+		log.Warnf("insign: %s", sign)
+		response.Error = "sign valication failed"
+		response.Status = STATUS_INVALID_PARAM
+		goto resp
+	}
+
+	if !checkAuthzUid(uid, rid) {
+		log.Warnf("auth failed. uid: %s, rid: %s", uid, rid)
+		response.Status = STATUS_OTHER_ERR
+		response.Error = "authorization failed"
+		goto resp
+	}
+
+	if r.Body == nil {
+		response.Status = STATUS_INVALID_PARAM
+		response.Error = "missing POST data"
+		goto resp
+	}
+
+	body, err = ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	if err != nil {
+		response.Status = STATUS_INVALID_PARAM
+		response.Error = "invalid POST body"
+		goto resp
+	}
+
+	if len(rid) == len("c80e774a1e78") {
+		// To old agent
+		type CommandRequest struct {
+			Uid string `json:"uid"`
+			Cmd string `json:"cmd"`
+		}
+		cmdRequest := CommandRequest{
+			Uid: uid,
+			Cmd: string(body),
+		}
+		bCmd, _ = json.Marshal(cmdRequest)
+		cmd = string(bCmd)
+		serviceName = "agent"
+	} else {
+		// To new android service
+		type RouterCommand struct {
+			Forward string `json:"forward"`
+		}
+		var rc RouterCommand
+		if err := json.Unmarshal([]byte(body), &rc); err != nil {
+			response.Status = -2000
+			response.Error = "Request body is not JSON"
+			goto resp
+		}
+		cmd = rc.Forward
+		if cmd == "" {
+			response.Status = -2001
+			response.Error = "'forward' is empty"
+			goto resp
+		}
+		serviceName = "agent" //FIXME
+	}
+
+	result, err = rpcClient.Control(rid, serviceName, cmd)
+	if err != nil {
+		if _, ok := err.(*mq.NoDeviceError); ok {
+			response.Status = STATUS_ROUTER_OFFLINE
+			response.Error = fmt.Sprintf("device (%s) offline", rid)
+		} else if _, ok := err.(*mq.TimeoutError); ok {
+			response.Status = STATUS_OTHER_ERR
+			response.Error = fmt.Sprintf("recv response timeout [%s]", rid)
+		} else if _, ok := err.(*mq.InvalidServiceError); ok {
+			response.Status = STATUS_OTHER_ERR
+			response.Error = fmt.Sprintf("Device [%s] has no service [%s]", rid, serviceName)
+		} else {
+			response.Status = STATUS_OTHER_ERR
+			response.Error = err.Error()
+		}
+	} else {
+		w.Write([]byte(result))
+	}
+
+resp:
+	b, _ := json.Marshal(response)
+	log.Debugf("postRouterCommand write: %s", string(b))
+	w.Write(b)
+}
+
+func getRouterList(w http.ResponseWriter, r *http.Request) {
+	log.Tracef("getRouterList")
+
+	type RouterInfo struct {
+		Rid   string `json:"rid"`
+		Rname string `json:"rname"`
+	}
+
+	type ResponseRouterList struct {
+		Status int          `json:"status"`
+		Descr  string       `json:"descr"`
+		List   []RouterInfo `json:"list"`
+	}
+
+	var (
+		uid      string
+		tid      string
+		sign     string
+		rid      string
+		src      string
+		tm       string
+		pmtt     string
+		path     string = "/router/list"
+		query    map[string]string
+		mysign   string
+		response ResponseRouterList
+		router   RouterInfo
+		devices  []devcenter.Device
+		err      error
+	)
+
+	response.Status = STATUS_OTHER_ERR
+	if r.Method != "GET" {
+		response.Descr = "must using 'GET' method\n"
+		goto resp
+	}
+	r.ParseForm()
+
+	uid = r.FormValue("uid")
+	if uid == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Descr = "missing 'uid'"
+		goto resp
+	}
+
+	tid = r.FormValue("tid")
+	if tid == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Descr = "missing 'tid'"
+		goto resp
+	}
+
+	sign = r.FormValue("sign")
+	if sign == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Descr = "missing 'sign'"
+		goto resp
+	}
+
+	tm = r.FormValue("tm")
+	if tm == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Descr = "missing 'tm'"
+		goto resp
+	}
+
+	pmtt = r.FormValue("pmtt")
+	if pmtt == "" {
+		response.Status = STATUS_INVALID_PARAM
+		response.Descr = "missing 'pmtt'"
+		goto resp
+	}
+
+	rid = r.FormValue("rid")
+	if rid == "" {
+		rid = "rid"
+	}
+
+	src = r.FormValue("src")
+	if src == "" {
+		src = "src"
+	}
+
+	query = map[string]string{
+		"uid":  uid,
+		"rid":  rid,
+		"tid":  tid,
+		"src":  src,
+		"tm":   tm,
+		"pmtt": pmtt,
+	}
+
+	mysign = sign_calc(path, query)
+	if mysign != sign {
+		log.Warnf("sign valication failed")
+		log.Warnf("mysign: %s", mysign)
+		log.Warnf("insign: %s", sign)
+		response.Descr = "sign valication failed"
+		response.Status = STATUS_INVALID_PARAM
+		goto resp
+	}
+
+	devices, err = devcenter.GetDevices(uid, devcenter.DEV_ROUTER)
+	if err != nil {
+		log.Errorf("GetDevices failed: %s", err.Error())
+		response.Descr = err.Error()
+		goto resp
+	}
+
+	for _, dev := range devices {
+		router = RouterInfo{
+			Rid:   dev.Id,
+			Rname: dev.Title,
+		}
+		response.List = append(response.List, router)
+	}
+
+	response.Status = 0
+	response.Descr = "OK"
+
+resp:
+	b, _ := json.Marshal(response)
+	log.Debugf("getRoutelist write: %s", string(b))
+	w.Write(b)
 }
